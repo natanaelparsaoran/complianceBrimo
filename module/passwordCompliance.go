bb5e517b1be900a93a501aaa42b24683ecd59548
addonrizky <adhon.rizky@gmail.com>
2021-03-08 10:36:02 +0700

    update compliance password

--- module/passwordCompliance.go
+++ module/passwordCompliance.go
@@ -24,7 +24,16 @@ func ComplyPassword(username string, usernameAlias string, password string, chan
 		if len(password) != 6 {
 			validationObject = model.Validation{
 				Code: "V6",
-				Desc: "Panjang minimal & Maksimal password adalah 6",
+				Desc: "Panjang minimal & Maksimal pin adalah 6",
+			}
+			return validationObject
+		}
+
+		//password must not contain only numeric
+		if !rule.IsNumeric(password) {
+			validationObject = model.Validation{
+				Code: "VH",
+				Desc: "pin harus angka",
 			}
 			return validationObject
 		}
@@ -35,7 +44,7 @@ func ComplyPassword(username string, usernameAlias string, password string, chan
 		if len(password) < 12 {
 			validationObject = model.Validation{
 				Code: "V6",
-				Desc: "Panjang minimal password adalah 6",
+				Desc: "Panjang minimal password adalah 12",
 			}
 			return validationObject
 		}
@@ -48,79 +57,79 @@ func ComplyPassword(username string, usernameAlias string, password string, chan
 			}
 			return validationObject
 		}
-	}
 
-	//password must not contain space
-	if rule.IsSpaceExist(password) {
-		validationObject = model.Validation{
-			Code: "V8",
-			Desc: "password tidak diperbolehkan mengandung karakter spasi",
-		}
-		return validationObject
-	}
-
-	//password must not contain only numeric
-	if rule.IsNumeric(password) {
-		validationObject = model.Validation{
-			Code: "V9",
-			Desc: "password tidak diperbolehkan hanya angka saja, harus mengandung minimal 1 karakter huruf kecil, 1 angka, dan 1 huruf besar.",
-		}
-		return validationObject
-	}
-
-	//password must not contain only alpha
-	if rule.IsAlphaOnly(password) {
-		validationObject = model.Validation{
-			Code: "VA",
-			Desc: "password tidak diperbolehkan hanya alfabet saja, harus mengandung minimal 1 karakter huruf kecil, 1 angka, dan 1 huruf besar.",
-		}
-		return validationObject
-	}
-
-	//password must contain alpha and numeric
-	if !rule.IsAlphaNumeric(password) {
-		validationObject = model.Validation{
-			Code: "VB",
-			Desc: "password harus mengandung alfa dan numeric",
-		}
-		return validationObject
-	}
-
-	//password must contain uppercase char
-	if !rule.IsUppercaseLetterExist(password) {
-		validationObject = model.Validation{
-			Code: "VC",
-			Desc: "password harus mengandung minimal 1 huruf besar",
-		}
-		return validationObject
-	}
-
-	//password must contain uppercase char
-	if !rule.IsLowercaseLetterExist(password) {
-		validationObject = model.Validation{
-			Code: "VD",
-			Desc: "password harus mengandung minimal 1 huruf kecil",
-		}
-		return validationObject
-	}
-
-	//first 4 char of password must not equal with first 4 char of username
-	if strings.ToUpper(username[0:4]) == strings.ToUpper(password[0:4]) {
-		validationObject = model.Validation{
-			Code: "VF",
-			Desc: "password tidak boleh mengandung unsur username",
-		}
-		return validationObject
-	}
-
-	//first 4 char of password must not equal with first 4 char of username alias
-	if isUsernameAliasExist {
-		if strings.ToUpper(usernameAlias[0:4]) == strings.ToUpper(password[0:4]) {
+		//password must not contain space
+		if rule.IsSpaceExist(password) {
 			validationObject = model.Validation{
-				Code: "VG",
+				Code: "V8",
+				Desc: "password tidak diperbolehkan mengandung karakter spasi",
+			}
+			return validationObject
+		}
+
+		//password must not contain only numeric
+		if rule.IsNumeric(password) {
+			validationObject = model.Validation{
+				Code: "V9",
+				Desc: "password tidak diperbolehkan hanya angka saja, harus mengandung minimal 1 karakter huruf kecil, 1 angka, dan 1 huruf besar.",
+			}
+			return validationObject
+		}
+
+		//password must not contain only alpha
+		if rule.IsAlphaOnly(password) {
+			validationObject = model.Validation{
+				Code: "VA",
+				Desc: "password tidak diperbolehkan hanya alfabet saja, harus mengandung minimal 1 karakter huruf kecil, 1 angka, dan 1 huruf besar.",
+			}
+			return validationObject
+		}
+
+		//password must contain alpha and numeric
+		if !rule.IsAlphaNumeric(password) {
+			validationObject = model.Validation{
+				Code: "VB",
+				Desc: "password harus mengandung alfa dan numeric",
+			}
+			return validationObject
+		}
+
+		//password must contain uppercase char
+		if !rule.IsUppercaseLetterExist(password) {
+			validationObject = model.Validation{
+				Code: "VC",
+				Desc: "password harus mengandung minimal 1 huruf besar",
+			}
+			return validationObject
+		}
+
+		//password must contain uppercase char
+		if !rule.IsLowercaseLetterExist(password) {
+			validationObject = model.Validation{
+				Code: "VD",
+				Desc: "password harus mengandung minimal 1 huruf kecil",
+			}
+			return validationObject
+		}
+
+		//first 4 char of password must not equal with first 4 char of username
+		if strings.ToUpper(username[0:4]) == strings.ToUpper(password[0:4]) {
+			validationObject = model.Validation{
+				Code: "VF",
 				Desc: "password tidak boleh mengandung unsur username",
 			}
 			return validationObject
+		}
+
+		//first 4 char of password must not equal with first 4 char of username alias
+		if isUsernameAliasExist {
+			if strings.ToUpper(usernameAlias[0:4]) == strings.ToUpper(password[0:4]) {
+				validationObject = model.Validation{
+					Code: "VG",
+					Desc: "password tidak boleh mengandung unsur username",
+				}
+				return validationObject
+			}
 		}
 	}
 
